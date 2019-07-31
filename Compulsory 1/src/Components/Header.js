38185d2088e1501d75d16c86581f247e6468d4fa
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-31 16:22:56 +0200

    Remove bad way

--- Compulsory 1/src/Components/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Logo from '../Images/logo.png'
-
-export default props => 
-	<header>
-		<img src={Logo} alt="logo" />
-		<h1>{props.loggedIn ? "Welcome" : "Please sign in"}</h1>
-	</header>
--- Compulsory 1/src/Components/Landing.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default props => (
-  <div className="landing">
-    <div className="title">{props.name}</div>
-    <div className="description">{props.children}</div>
-  </div>
-)
